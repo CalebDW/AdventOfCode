01cde66728d2a7fc13d4be97552ecd59ea658a4e
Caleb White <cdwhite3@pm.me>
2023-12-09 23:00:55 -0600

    Day 9 php

---
+++ 2023/php/app/Days/Day09.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Days;
+
+class Day09 extends AocDay
+{
+    public function partOne(): mixed
+    {
+        return $this->lines
+            ->filter()
+            ->map(fn ($line) => $this->extrapolate(array_map('intval', explode(' ', $line))))
+            ->sum();
+    }
+
+    public function partTwo(): mixed
+    {
+        return $this->lines
+            ->filter()
+            ->map(fn ($line) => $this->extrapolate(array_map('intval', explode(' ', $line)), forwards: false))
+            ->sum();
+    }
+
+    /** @param array<int, int> $points */
+    protected function extrapolate(array $points, bool $forwards = true): int
+    {
+        if (count(array_filter($points, fn ($point) => $point !== 0)) === 0) {
+            return 0;
+        }
+
+        $difference = [];
+
+        for ($i = 0; $i < count($points) - 1; $i++) {
+            $difference[] = $points[$i + 1] - $points[$i];
+        }
+
+        return $forwards
+            ? end($points) + $this->extrapolate($difference, $forwards)
+            : $points[0] - $this->extrapolate($difference, $forwards);
+    }
+}
