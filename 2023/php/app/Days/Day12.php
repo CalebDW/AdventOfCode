e4d0baccc6a044e8b4dd70b06a5cc22e9c5d858b
Caleb White <cdwhite3@pm.me>
2024-11-18 08:08:11 -0600

    wip: 2023 day 12

---
+++ 2023/php/app/Days/Day12.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Days;
+
+use Illuminate\Support\Collection;
+
+class Day12 extends AocDay
+{
+    /** @var Collection<int, springs> */
+    protected Collection $springs;
+
+    protected function parseInput(string $input): void
+    {
+        parent::parseInput($input);
+
+        $this->springs = $this->lines
+            ->filter()
+            ->map(function ($line) {
+                $row = explode(' ', $line);
+
+                return new Springs(
+                    row: $row[0],
+                    damaged: collect(explode(',', $row[1])),
+                );
+        });
+    }
+
+    public function partOne(): mixed
+    {
+        return $this->springs
+            ->map(fn ($spring) => $spring->arrangements())
+            ->sum();
+    }
+
+    public function partTwo(): mixed
+    {
+        return null;
+    }
+
+}
+
+final class Springs
+{
+    public function __construct(
+        public string $row,
+        /** @var Collection<int, int> */
+        public Collection $damaged,
+    ) {
+    }
+
+    public function arrangements(): int
+    {
+        return preg_match_all($this->regex(), $this->row, $matches);
+
+        dd($matches);
+    }
+
+    private function regex(): string
+    {
+        $groups = $this->damaged
+            ->map(fn ($group) => "[#?]{{$group}}")
+            ->join("[.?]+");
+
+        return "/(?=([.?]*{$groups}[.?]*))/";
+    }
+}
