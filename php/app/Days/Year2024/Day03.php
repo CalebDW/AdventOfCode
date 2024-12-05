f6038a2b640d0e5428ae29eb723609f2614375e1
Caleb White <cdwhite3@pm.me>
2024-12-04 22:39:03 -0600

    feat(php): 2024 day 3

---
+++ php/app/Days/Year2024/Day03.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Days\Year2024;
+
+use App\Days\AocDay;
+
+class Day03 extends AocDay
+{
+    private string $input;
+
+    protected function parseInput(string $input): void
+    {
+        $this->input = $input;
+    }
+
+    public function partOne(): ?string
+    {
+        preg_match_all('/mul\((\d+,\d+)\)/', $this->input, $matches);
+
+        return (string) collect($matches[1])
+            ->map(fn ($m) => array_product(explode(',', $m)))
+            ->sum();
+    }
+
+    public function partTwo(): ?string
+    {
+        preg_match_all("/mul\(\d+,\d+\)|do\(\)|don't\(\)/", $this->input, $matches);
+
+        $matches = array_reverse($matches[0]);
+
+        $result = 0;
+        $condition = true;
+
+        while (key($matches) !== null) {
+            $value = array_pop($matches);
+
+            if (str_contains($value, 'do')) {
+                $condition = $value === 'do()';
+
+                continue;
+            }
+
+            $value = str_replace(['mul(', ')'], '', $value);
+
+            if ($condition) {
+                $result += array_product(explode(',', $value));
+            }
+        }
+
+        return (string) $result;
+    }
+}
